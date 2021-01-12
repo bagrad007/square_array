ef06eee8a461c75ec9b9c7593e6da80c866d8b32
Luke Gill <bagrad007@gmail.com>
2021-01-12 14:37:48 -0500

    commit tree 47cd55b6d7b91df0cd35f650941f31620adbd132

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def square_array(array)
   # your code here
+ 
+  empty_array = []
+  array.each { |num| 
+    empty_array << num ** 2
+    
+    # binding.pry 
+}
+  empty_array
+   
 end
